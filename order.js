154c8530379e03db9a5cea9ad5bcb9fadf6b8e3c
Jihan4105 <wlgks6979@naver.com>
2024-10-25 11:52:31 +0900

    "2024-10-25 Development(end)"

---
+++ order.js
@@ -0,0 +1,32 @@
+const Orders = [
+  {
+    productName: 'JavaScript Tutorials',
+    productNumber: '85743',
+    paymentStatus: 'Due',
+    status: 'Pending'
+  },
+  {
+    productName: 'CSS Full Course',
+    productNumber: '97243',
+    paymentStatus: 'Refunded',
+    status: 'Declined'
+  },
+  {
+    productName: 'Flex-Box Tutorial',
+    productNumber: '28462',
+    paymentStatus: 'Paid',
+    status: 'Active'
+  },
+  {
+    productName: 'JavaScript Tutorials',
+    productNumber: '85743',
+    paymentStatus: 'Due',
+    status: 'Pending'
+  },
+  {
+    productName: 'CSS Full Course',
+    productNumber: '97243',
+    paymentStatus: 'Refunded',
+    status: 'Declined'
+  }
+]
